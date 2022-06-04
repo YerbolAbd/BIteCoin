473db974ee16c4a87e6f7d8e4b5ba147306bae27
YerbolAbd <abdiyevyerbol@yahoo.com>
2022-06-04 11:23:23 +0600

    Initial Commit

---
+++ BiteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BiteCoin
+//
+//  Created by Ербол on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
